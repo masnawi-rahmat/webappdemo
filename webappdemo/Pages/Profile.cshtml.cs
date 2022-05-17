98290746894d6357710a40823bff69430353cede
masnawi-rahmat <masnawi.rahmat@gmail.com>
2022-05-17 09:39:54 +0800

    Add project files.

---
+++ webappdemo/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webappdemo.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
